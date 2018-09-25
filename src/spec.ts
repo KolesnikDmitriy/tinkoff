449821f99e172d5df43c4637aa7bb801f2e8e009
dkolesnik <dmitrykolesnik1@gmail.com>
2018-09-25 21:39:27 +0300

    add first navigate step

--- src/spec.ts
+++ src/spec.ts
@@ -1,10 +1,17 @@
 import 'jasmine';
-import { browser } from 'protractor';
+import { browser, element, by, ExpectedConditions } from 'protractor';
 
 describe('A suite', () => {
   it('contains spec with an expectation', () => {
+    browser.waitForAngularEnabled(false);
     browser.get('https://tinkoff.ru');
+    const paymants = element(by.linkText('Платежи'));
+    paymants.click();
 
-    expect(true).toBe(true);
+    browser.wait(ExpectedConditions.urlIs('https://www.tinkoff.ru/payments/'), 5000);
+
+    browser.getCurrentUrl().then(url => {
+      console.log(url);
+    });
   });
 });
